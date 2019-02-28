151f96367ea519c31297ebf670a424ff7f484b2c
JaviMuru <javimuruzabal@hotmail.com>
2019-02-28 22:06:41 +0100

    Inital commit
    Signed-off-by: JaviMuru <javimuruzabal@hotmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+# Environment Variables
+
+The project needs the following variables:
+
+**API_KEY**
+
+**WEATHER_URL**
+
 ## Available Scripts
 
 In the project directory, you can run:
---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Titles extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className = "title-continer__title">Weather App</h1>
+        <h3 className = "title-continer__subtitle">Find out temperature, conditions and more...</h3>
+      </div>
+    )
+  }
+}
+
+export default Titles;
